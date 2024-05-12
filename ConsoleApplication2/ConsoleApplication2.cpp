8336544a39261f60cbf3285abff39f5335a0cbd8
Lxnkk <zavideyplaton@gmail.com>
2024-05-12 10:52:15 +0300

    Add files via upload

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,8 +1,17 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!";
+	vector <int> v{ 1, 3, 6, 4, 2, 5 };
+	sort(v.begin(), v.end());
+
+	cout << "sorted vector:";
+	for (auto x : v)
+		cout << x << " ";
+	return 0;
+
 }
